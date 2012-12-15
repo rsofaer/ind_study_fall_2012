60da4ec2408dec51823f3f1aaeed1982fc8fb96e
Raphael Sofaer <raphael@nyuacmdell755.(none)>
2012-12-14 23:47:06 -0500

    Add some matricies, need to use trilinos 11.0 to read fucking matrixmarket symmetric matricies --- buuuulllllshit

--- cppTree/src/main.cpp
+++ cppTree/src/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
 
-#ifdef MYAPP_MPI
-#include "Teuchos_DefaultSerialComm.hpp"
-#endif
 
 #ifdef MYAPP_EPETRA
 #include "Epetra_SerialDenseVector.h"
 #endif
 
-
-
+#include "Epetra_SerialComm.h"
+#include "Epetra_CrsMatrix.h"
 int main(int argc, char *argv[]) {
- std::cout << "it compiles" << std::endl;
+ Epetra_Object::SetTracebackMode(2);
+ Epetra_SerialComm Comm;
+ char *mtx_filename = argv[1];
+ std::cout << mtx_filename << std::endl;
+ Epetra_CrsMatrix* mat;
+ int res = EpetraExt::MatrixMarketFileToCrsMatrix(mtx_filename, Comm, mat, false, true);
+ std::cout << res << ", " << errno << ", " << mat << std::endl;
+ //std::cout << mat->NormOne() << std::endl;
+
 }
