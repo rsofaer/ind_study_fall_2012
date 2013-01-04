f07920b2fed5bf9a12917701c09aefaa5e973858
Raphael Sofaer <raphael@joindiaspora.com>
2013-01-03 23:21:17 -0800

    Working cmakelists

--- cppTree/src/main.cpp
+++ cppTree/src/main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
-
-#ifdef MYAPP_EPETRA
-#include "Epetra_SerialDenseVector.h"
-#endif
-
-#include "Epetra_SerialComm.h"
-#include "Epetra_CrsMatrix.h"
-int main(int argc, char *argv[]) {
- Epetra_Object::SetTracebackMode(2);
- Epetra_SerialComm Comm;
- char *mtx_filename = argv[1];
- std::cout << mtx_filename << std::endl;
- Epetra_CrsMatrix* mat;
- int res = EpetraExt::MatrixMarketFileToCrsMatrix(mtx_filename, Comm, mat, false, true);
- std::cout << res << ", " << errno << ", " << mat << std::endl;
- //std::cout << mat->NormOne() << std::endl;
-
+int main(int argc, char* argv[])
+{
+  return 0;
 }
+
